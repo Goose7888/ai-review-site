4bb4ad504f43f1f32c9f91cf3f069e73106a8c65
Goose <tlclark1031@gmail.com>
2025-03-24 15:10:39 -0500

    Some Button Logic (Validation)

---
+++ scripts/listeners.js
@@ -0,0 +1,41 @@
+
+document.querySelector('#btnSubmitLogin').addEventListener('click', function() {
+    const regEmail = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
+
+    const strEmail = document.querySelector('#txtLoginEmail').value
+    const strPassword = document.querySelector('#txtLoginPassword').value
+
+    // Validate Login info
+    let blnError = false
+
+    if (!regEmail.test())
+    {
+        console.log('Email is invalid')
+        blnError = true
+    }
+
+    // no whitespace, 8 or longer chars
+    if (strPassword.includes(' ') || strPassword.length < 8)
+    {
+        console.log('Password is invalid')
+        blnError = true
+    }
+    
+
+    if (blnError)
+    {
+        console.log('Hey we have an error')
+        exit()
+    }
+
+
+    // If validation goes well (we have a yes) do the rest of the function from here
+
+
+    
+
+
+    // POSTs login to backend
+        // If response is yes, let the user in the gates
+        // If response is no yes, tell them no yes
+})
